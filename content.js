105fb91ac7fcc7f8f95a7bd4a08299c1137ee493
GURU SENSEI <sahoogurucharan00@gmail.com>
2025-08-25 10:39:53 +0000

    class logic into a separate function.

--- content.js
+++ content.js
@@ -1,11 +1,17 @@
-document.body.classList.add("block-link-previews");
+const setBlockingClass = (enabled) => {
+  if (enabled) {
+    document.body.classList.add("block-link-previews");
+  } else {
+    document.body.classList.remove("block-link-previews");
+  }
+};
+
+chrome.storage.local.get("isEnabled", (data) => {
+  setBlockingClass(data.isEnabled);
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "toggle") {
-    if (message.isEnabled) {
-      document.body.classList.add("block-link-previews");
-    } else {
-      document.body.classList.remove("block-link-previews");
-    }
+    setBlockingClass(message.isEnabled);
   }
 });
