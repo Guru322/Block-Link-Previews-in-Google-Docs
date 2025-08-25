9a412fadfd3e4ce1191f4db02e68f19e1a475f8f
GURU SENSEI <sahoogurucharan00@gmail.com>
2025-08-25 10:33:24 +0000

    Set default state to enabled on extension installation

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ isEnabled: false });
+  chrome.storage.local.set({ isEnabled: true });
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
